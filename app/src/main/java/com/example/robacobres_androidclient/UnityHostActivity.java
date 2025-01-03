6c0f6294913599e8a3a8787b3548d9ab49c036b2
FalcoLluc <joan.lluc.fernandez@estudiantat.upc.edu>
2025-01-03 12:15:26 +0100

    Unity_Integracion_2

--- app/src/main/java/com/example/robacobres_androidclient/UnityHostActivity.java
+++ app/src/main/java/com/example/robacobres_androidclient/UnityHostActivity.java
@@ -33,26 +33,12 @@ public class UnityHostActivity extends UnityPlayerActivity implements UnityCallb
         serviceREST.sendStateToServer(itemsStateText,this);
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finish();  // This will finish UnityHostActivity and return to MultiActivity
+    // Expose this method to Unity to close the activity
+    public void exitUnity() {
+        finish();  // This will close the UnityHostActivity and return to the previous activity
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // Make sure Unity is resumed when the app comes back from the background.
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // Handle when Unity goes to the background, save game state if needed.
-    }
-
-    // Other lifecycle methods like onStop, onDestroy can be overridden if necessary.
-
+    //CALLBACKS
     @Override
     public void onReturnHola() {
         UnityPlayer.UnitySendMessage("AndroidBridge", "OnServerResponse", "ECO HOLA");
